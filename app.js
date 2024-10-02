28fe56cf73c3cd79936b22515f02409ee6bfecae
JTabaries <59892925+JTabaries@users.noreply.github.com>
2024-10-02 18:06:37 +0200

    Fin partie 1

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.post('/api/stuff', (req,res,next) =>{
+  console.log(req.body);
+  res.status(201).json({
+    message: 'objet créé'
+  });
+});
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content, Accespt, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,PATCH,OPTIONS');
+  next();
+});
+
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
       {
